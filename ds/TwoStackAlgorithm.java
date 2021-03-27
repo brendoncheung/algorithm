193c67438f18a57fa8dd8b8058cf0368e4a99113
Brendon Cheung <brendon.cheung@outlook.com>
2021-03-27 18:38:10 -0400

    completed TwoStackAlgorithm

--- ds/TwoStackAlgorithm.java
+++ ds/TwoStackAlgorithm.java
@@ -1,20 +1,34 @@
-class TwoStackAlgorithm {
+public class TwoStackAlgorithm {
 
-	Stack<String> s1 = new Stack<String>(); //operand stack
-	Stack<Double> s2 = new Stack<Double>(); //operator stack
-	
-	public int evaluate(String s) {
-		for(char c : s) {
-			
+	/**
+	 * Ignore LEFT parenthesis
+	 * Push operand into operand stack
+	 * Push operator into operator stack
+	 * When RIGHT parenthesis, pop operator and apply the first two operand and push back to the stack
+	 */
+
+	Stack<Character> operator = new Stack<Character>(); //operand stack
+	Stack<Double> operand = new Stack<Double>(); //operator stack
+	public Double evaluate(String s) {
+
+		for(char c : s.toCharArray()) {
+
+			if(c == '(');
+			else if (c == '+') operator.push('+');
+			else if (c == '-') operator.push('-');
+			else if (c == '*') operator.push('*');
+			else if (c == '/') operator.push('/');
+			else if (c == ')') {
+				Character op = operator.pop();
+				Double d = operand.pop();
+				if(op == '+') d = operand.pop() + d;
+				else if (op == '-') d = operand.pop() - d;
+				else if (op == '*') d = operand.pop() * d;
+				else if (op == '/') d = operand.pop() / d;
+				operand.push(d);
+			} else operand.push(Double.parseDouble(String.valueOf(c)));
 		}
-		
-		
+		return operand.pop();
 	}
-	
-	private char[] toChar(String s) {
-		return s.
-		
-	}
-	
-	
+
 }
